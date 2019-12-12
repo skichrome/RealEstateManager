3796305505c17856941d697fd3626a59858b25bc
skichrome <toni.campeol@outlook.fr>
2019-12-12 01:12:06 +0100

    [Stability] Fixed UI test of SignInActivityUITests.kt

--- app/src/androidTest/java/com/skichrome/realestatemanager/SignInActivityUITests.kt
+++ app/src/androidTest/java/com/skichrome/realestatemanager/SignInActivityUITests.kt
@@ -6,8 +6,11 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
+import com.skichrome.realestatemanager.utils.AppCoroutinesConfiguration
 import com.skichrome.realestatemanager.view.SignInActivity
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,10 +22,26 @@ class SignInActivityUITests
     @get:Rule
     var activityTestRule = ActivityTestRule(SignInActivity::class.java)
 
+    companion object
+    {
+        @BeforeClass
+        @JvmStatic
+        fun initCoroutines()
+        {
+            with(AppCoroutinesConfiguration) {
+                uiDispatchers = Dispatchers.Main
+                backgroundDispatchers = Dispatchers.Unconfined
+                ioDispatchers = Dispatchers.Unconfined
+            }
+        }
+    }
+
     @Test
-    fun assertFirstScreen() = runBlocking {
-        Espresso.onView(withText(R.string.activity_sign_in_hint))
-            .check(matches(isDisplayed()))
-            .let { Unit }
+    fun assertFirstScreen()
+    {
+        runBlocking {
+            Espresso.onView(withText(R.string.activity_sign_in_hint))
+                .check(matches(isDisplayed()))
+        }
     }
 }
